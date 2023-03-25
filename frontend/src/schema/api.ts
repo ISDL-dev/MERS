916506ae0e06825e37127564327953f977ff89df
hoka <oka.hayato@mikilab.doshisha.ac.jp>
2023-03-25 13:10:30 +0900

    add tags and operationId #10

--- frontend/src/schema/api.ts
+++ frontend/src/schema/api.ts
@@ -95,6 +95,220 @@ export interface Movie {
 /**
  * 
  * @export
+ * @interface PostTrialsRequest
+ */
+export interface PostTrialsRequest {
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequest
+     */
+    'datetime'?: string;
+    /**
+     * 
+     * @type {PostTrialsRequestSubject}
+     * @memberof PostTrialsRequest
+     */
+    'subject': PostTrialsRequestSubject;
+    /**
+     * 
+     * @type {Array<PostTrialsRequestRatingsInner>}
+     * @memberof PostTrialsRequest
+     */
+    'ratings': Array<PostTrialsRequestRatingsInner>;
+}
+/**
+ * 
+ * @export
+ * @interface PostTrialsRequestRatingsInner
+ */
+export interface PostTrialsRequestRatingsInner {
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInner
+     */
+    'media_id'?: number;
+    /**
+     * 
+     * @type {PostTrialsRequestRatingsInnerEmotions}
+     * @memberof PostTrialsRequestRatingsInner
+     */
+    'emotions'?: PostTrialsRequestRatingsInnerEmotions;
+}
+/**
+ * 
+ * @export
+ * @interface PostTrialsRequestRatingsInnerEmotions
+ */
+export interface PostTrialsRequestRatingsInnerEmotions {
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInnerEmotions
+     */
+    'valence'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInnerEmotions
+     */
+    'arousal'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInnerEmotions
+     */
+    'liking'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInnerEmotions
+     */
+    'dominance'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestRatingsInnerEmotions
+     */
+    'famility'?: number;
+}
+/**
+ * 
+ * @export
+ * @interface PostTrialsRequestSubject
+ */
+export interface PostTrialsRequestSubject {
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'participant_id'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'age'?: number;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'gender'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'handedness'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'vision'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'vision_Aid'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'education'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'alcohol_consumption'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'coffee_consumption'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'tea_consumption'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'tobacco_consumption'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'drug_consumption'?: string;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'syndroms'?: string;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'hours_of_sleep_last_night'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'normal_hours_of_sleep'?: number;
+    /**
+     * 
+     * @type {string}
+     * @memberof PostTrialsRequestSubject
+     */
+    'level_of_alertness'?: string;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof PostTrialsRequestSubject
+     */
+    'distribute_physiological_recordings'?: boolean;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof PostTrialsRequestSubject
+     */
+    'publish_audio_visual_recordings'?: boolean;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'head_circumference'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'distance_nasion-inion'?: number;
+    /**
+     * 
+     * @type {number}
+     * @memberof PostTrialsRequestSubject
+     */
+    'distance_left_right_jaw_hinge'?: number;
+}
+/**
+ * 
+ * @export
  * @interface TargetsInner
  */
 export interface TargetsInner {
@@ -111,325 +325,6 @@ export interface TargetsInner {
      */
     'ids'?: Array<string>;
 }
-/**
- * 
- * @export
- * @interface TrialsPostRequest
- */
-export interface TrialsPostRequest {
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequest
-     */
-    'datetime'?: string;
-    /**
-     * 
-     * @type {TrialsPostRequestSubject}
-     * @memberof TrialsPostRequest
-     */
-    'subject': TrialsPostRequestSubject;
-    /**
-     * 
-     * @type {Array<TrialsPostRequestRatingsInner>}
-     * @memberof TrialsPostRequest
-     */
-    'ratings': Array<TrialsPostRequestRatingsInner>;
-}
-/**
- * 
- * @export
- * @interface TrialsPostRequestRatingsInner
- */
-export interface TrialsPostRequestRatingsInner {
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInner
-     */
-    'media_id'?: number;
-    /**
-     * 
-     * @type {TrialsPostRequestRatingsInnerEmotions}
-     * @memberof TrialsPostRequestRatingsInner
-     */
-    'emotions'?: TrialsPostRequestRatingsInnerEmotions;
-}
-/**
- * 
- * @export
- * @interface TrialsPostRequestRatingsInnerEmotions
- */
-export interface TrialsPostRequestRatingsInnerEmotions {
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInnerEmotions
-     */
-    'valence'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInnerEmotions
-     */
-    'arousal'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInnerEmotions
-     */
-    'liking'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInnerEmotions
-     */
-    'dominance'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestRatingsInnerEmotions
-     */
-    'famility'?: number;
-}
-/**
- * 
- * @export
- * @interface TrialsPostRequestSubject
- */
-export interface TrialsPostRequestSubject {
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'participant_id'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'age'?: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'gender'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'handedness'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'vision'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'vision_Aid'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'education'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'alcohol_consumption'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'coffee_consumption'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'tea_consumption'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'tobacco_consumption'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'drug_consumption'?: string;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'syndroms'?: string;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'hours_of_sleep_last_night'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'normal_hours_of_sleep'?: number;
-    /**
-     * 
-     * @type {string}
-     * @memberof TrialsPostRequestSubject
-     */
-    'level_of_alertness'?: string;
-    /**
-     * 
-     * @type {boolean}
-     * @memberof TrialsPostRequestSubject
-     */
-    'distribute_physiological_recordings'?: boolean;
-    /**
-     * 
-     * @type {boolean}
-     * @memberof TrialsPostRequestSubject
-     */
-    'publish_audio_visual_recordings'?: boolean;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'head_circumference'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'distance_nasion-inion'?: number;
-    /**
-     * 
-     * @type {number}
-     * @memberof TrialsPostRequestSubject
-     */
-    'distance_left_right_jaw_hinge'?: number;
-}
-
-/**
- * DefaultApi - axios parameter creator
- * @export
- */
-export const DefaultApiAxiosParamCreator = function (configuration?: Configuration) {
-    return {
-        /**
-         * create a trial result containing a set of a subject info and rating results
-         * @summary create a trial result
-         * @param {TrialsPostRequest} [trialsPostRequest] 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        trialsPost: async (trialsPostRequest?: TrialsPostRequest, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
-            const localVarPath = `/trials`;
-            // use dummy base URL string because the URL constructor only accepts absolute URLs.
-            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
-            let baseOptions;
-            if (configuration) {
-                baseOptions = configuration.baseOptions;
-            }
-
-            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
-            const localVarHeaderParameter = {} as any;
-            const localVarQueryParameter = {} as any;
-
-
-    
-            localVarHeaderParameter['Content-Type'] = 'application/json';
-
-            setSearchParams(localVarUrlObj, localVarQueryParameter);
-            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
-            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
-            localVarRequestOptions.data = serializeDataIfNeeded(trialsPostRequest, localVarRequestOptions, configuration)
-
-            return {
-                url: toPathString(localVarUrlObj),
-                options: localVarRequestOptions,
-            };
-        },
-    }
-};
-
-/**
- * DefaultApi - functional programming interface
- * @export
- */
-export const DefaultApiFp = function(configuration?: Configuration) {
-    const localVarAxiosParamCreator = DefaultApiAxiosParamCreator(configuration)
-    return {
-        /**
-         * create a trial result containing a set of a subject info and rating results
-         * @summary create a trial result
-         * @param {TrialsPostRequest} [trialsPostRequest] 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        async trialsPost(trialsPostRequest?: TrialsPostRequest, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
-            const localVarAxiosArgs = await localVarAxiosParamCreator.trialsPost(trialsPostRequest, options);
-            return createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration);
-        },
-    }
-};
-
-/**
- * DefaultApi - factory interface
- * @export
- */
-export const DefaultApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
-    const localVarFp = DefaultApiFp(configuration)
-    return {
-        /**
-         * create a trial result containing a set of a subject info and rating results
-         * @summary create a trial result
-         * @param {TrialsPostRequest} [trialsPostRequest] 
-         * @param {*} [options] Override http request option.
-         * @throws {RequiredError}
-         */
-        trialsPost(trialsPostRequest?: TrialsPostRequest, options?: any): AxiosPromise<void> {
-            return localVarFp.trialsPost(trialsPostRequest, options).then((request) => request(axios, basePath));
-        },
-    };
-};
-
-/**
- * DefaultApi - object-oriented interface
- * @export
- * @class DefaultApi
- * @extends {BaseAPI}
- */
-export class DefaultApi extends BaseAPI {
-    /**
-     * create a trial result containing a set of a subject info and rating results
-     * @summary create a trial result
-     * @param {TrialsPostRequest} [trialsPostRequest] 
-     * @param {*} [options] Override http request option.
-     * @throws {RequiredError}
-     * @memberof DefaultApi
-     */
-    public trialsPost(trialsPostRequest?: TrialsPostRequest, options?: AxiosRequestConfig) {
-        return DefaultApiFp(this.configuration).trialsPost(trialsPostRequest, options).then((request) => request(this.axios, this.basePath));
-    }
-}
-
 
 /**
  * ImagesApi - axios parameter creator
@@ -749,6 +644,111 @@ export class TargetsApi extends BaseAPI {
      */
     public getTargets(type: string, num: number, options?: AxiosRequestConfig) {
         return TargetsApiFp(this.configuration).getTargets(type, num, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+/**
+ * TrialsApi - axios parameter creator
+ * @export
+ */
+export const TrialsApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * create a trial result containing a set of a subject info and rating results
+         * @summary create a trial result
+         * @param {PostTrialsRequest} [postTrialsRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        postTrials: async (postTrialsRequest?: PostTrialsRequest, options: AxiosRequestConfig = {}): Promise<RequestArgs> => {
+            const localVarPath = `/trials`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(postTrialsRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * TrialsApi - functional programming interface
+ * @export
+ */
+export const TrialsApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = TrialsApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * create a trial result containing a set of a subject info and rating results
+         * @summary create a trial result
+         * @param {PostTrialsRequest} [postTrialsRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async postTrials(postTrialsRequest?: PostTrialsRequest, options?: AxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.postTrials(postTrialsRequest, options);
+            return createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration);
+        },
+    }
+};
+
+/**
+ * TrialsApi - factory interface
+ * @export
+ */
+export const TrialsApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = TrialsApiFp(configuration)
+    return {
+        /**
+         * create a trial result containing a set of a subject info and rating results
+         * @summary create a trial result
+         * @param {PostTrialsRequest} [postTrialsRequest] 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        postTrials(postTrialsRequest?: PostTrialsRequest, options?: any): AxiosPromise<void> {
+            return localVarFp.postTrials(postTrialsRequest, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * TrialsApi - object-oriented interface
+ * @export
+ * @class TrialsApi
+ * @extends {BaseAPI}
+ */
+export class TrialsApi extends BaseAPI {
+    /**
+     * create a trial result containing a set of a subject info and rating results
+     * @summary create a trial result
+     * @param {PostTrialsRequest} [postTrialsRequest] 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof TrialsApi
+     */
+    public postTrials(postTrialsRequest?: PostTrialsRequest, options?: AxiosRequestConfig) {
+        return TrialsApiFp(this.configuration).postTrials(postTrialsRequest, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
