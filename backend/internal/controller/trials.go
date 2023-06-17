5f7a0da9497c5a123a6646366e66927b6e08e74d
be3 <d_0711@icloud.com>
2023-06-17 19:42:57 +0900

    Merge into main

--- backend/internal/controller/trials.go
+++ backend/internal/controller/trials.go
@@ -2,13 +2,14 @@ package controller
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"github.com/hoka-isdl/MERS/backend/internal/schema"
 	"github.com/hoka-isdl/MERS/backend/internal/repository"
+	"github.com/hoka-isdl/MERS/backend/internal/schema"
 )
 
 func PostTrialsHandlerFunc(ctx *gin.Context) {
-	var trials openapi.TrialsPostRequest
+	var trials schema.PostTrialsRequest
 
 	if err := ctx.BindJSON(&trials); err != nil {
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -17,7 +18,7 @@ func PostTrialsHandlerFunc(ctx *gin.Context) {
 
 	// repository.CreateMediasRecords()//Mediasテーブルに仮の情報を挿入.Mediasテーブルが作成でき次第削除予定
 	repository.CreateSubjectsRecords(trials.Subject)
-	repository.CreateEmotionsRecords(trials.Subject,trials.Ratings)
+	repository.CreateEmotionsRecords(trials.Subject, trials.Ratings)
 
 	ctx.JSON(200, gin.H{
 		"message": "success",
--- backend/internal/repository/post_trials.go
+++ backend/internal/repository/post_trials.go
@@ -2,43 +2,44 @@ package repository
 
 import (
 	"log"
+
 	"github.com/hoka-isdl/MERS/backend/internal/schema"
 )
 
-func CreateSubjectsRecords(subject openapi.TrialsPostRequestSubject) {
-	insert, err := db.Prepare("INSERT INTO Subjects(age,gender,handedness,vision,vision_aid,education,alcohol_consumption,"+
-		"coffee_consumption,tea_consumption,tobacco_consumption,drug_consumption,syndroms,hours_of_sleep_last_night,normal_hours_of_sleep,"+
-		"level_of_alertness,distribute_physiological_recordings,publish_audio_visual_recordings,head_circumference,distance_nasion_inion,distance_left_right_jaw_hinge) "+
+func CreateSubjectsRecords(subject schema.PostTrialsRequestSubject) {
+	insert, err := db.Prepare("INSERT INTO Subjects(age,gender,handedness,vision,vision_aid,education,alcohol_consumption," +
+		"coffee_consumption,tea_consumption,tobacco_consumption,drug_consumption,syndroms,hours_of_sleep_last_night,normal_hours_of_sleep," +
+		"level_of_alertness,distribute_physiological_recordings,publish_audio_visual_recordings,head_circumference,distance_nasion_inion,distance_left_right_jaw_hinge) " +
 		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	insert.Exec(subject.Age,subject.Gender,subject.Handedness,subject.Vision,subject.VisionAid,subject.Education,
-		subject.AlcoholConsumption,subject.CoffeeConsumption,subject.TeaConsumption,subject.TobaccoConsumption,
-		subject.DrugConsumption,subject.Syndroms,subject.HoursOfSleepLastNight,subject.NormalHoursOfSleep,
-		subject.LevelOfAlertness,subject.DistributePhysiologicalRecordings,subject.PublishAudioVisualRecordings,
-		subject.HeadCircumference,subject.DistanceNasionInion,subject.DistanceLeftRightJawHinge)
+	insert.Exec(subject.Age, subject.Gender, subject.Handedness, subject.Vision, subject.VisionAid, subject.Education,
+		subject.AlcoholConsumption, subject.CoffeeConsumption, subject.TeaConsumption, subject.TobaccoConsumption,
+		subject.DrugConsumption, subject.Syndroms, subject.HoursOfSleepLastNight, subject.NormalHoursOfSleep,
+		subject.LevelOfAlertness, subject.DistributePhysiologicalRecordings, subject.PublishAudioVisualRecordings,
+		subject.HeadCircumference, subject.DistanceNasionInion, subject.DistanceLeftRightJawHinge)
 }
 
-func CreateEmotionsRecords(subject openapi.TrialsPostRequestSubject, rating []openapi.TrialsPostRequestRatingsInner) {
+func CreateEmotionsRecords(subject schema.PostTrialsRequestSubject, rating []schema.PostTrialsRequestRatingsInner) {
 	insert, err := db.Prepare("INSERT INTO Emotions(participant_id,media_id,valence,arousal,liking,dominance,famility) VALUES(?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	for i := 0; i < len(rating); i++{
-		insert.Exec(subject.ParticipantId,rating[i].MediaId,rating[i].Emotions.Valence,
-			rating[i].Emotions.Arousal,rating[i].Emotions.Liking,
-			rating[i].Emotions.Dominance,rating[i].Emotions.Famility)
+	for i := 0; i < len(rating); i++ {
+		insert.Exec(subject.ParticipantId, rating[i].MediaId, rating[i].Emotions.Valence,
+			rating[i].Emotions.Arousal, rating[i].Emotions.Liking,
+			rating[i].Emotions.Dominance, rating[i].Emotions.Famility)
 	}
 }
 
-func CreateMediasRecords(){
+func CreateMediasRecords() {
 	insert, err := db.Prepare("INSERT INTO Medias(type,filename) VALUES(?, ?)")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	insert.Exec("image","sample.jpg")
+	insert.Exec("image", "sample.jpg")
 }
