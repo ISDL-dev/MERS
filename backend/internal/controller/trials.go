dc5ca2c6f88113d98ee80f3cff493597ea6b5bf7
IRIS <github@mikilab.doshisha.ac.jp>
2023-09-12 18:34:29 +0900

    fix: change table name & handle error

--- backend/internal/controller/trials.go
+++ backend/internal/controller/trials.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,13 +14,24 @@ func PostTrialsHandlerFunc(ctx *gin.Context) {
 	var trials schema.PostTrialsRequest
 
 	if err := ctx.BindJSON(&trials); err != nil {
+		log.Printf("Internal Server Error: failed to bind a request body with a struct: %v\n", err)
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
+	fmt.Println(trials)
 	// repository.CreateMediasRecords()//Mediasテーブルに仮の情報を挿入.Mediasテーブルが作成でき次第削除予定
-	repository.CreateSubjectsRecords(trials.Subject)
-	repository.CreateEmotionsRecords(trials.Subject, trials.Ratings)
+	if err := repository.CreateSubjectsRecords(trials.Subject); err != nil {
+		log.Printf("Internal Server Error: failed to create a subject record: %v\n", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	if err := repository.CreateEmotionsRecords(trials.Subject, trials.Ratings); err != nil {
+		log.Printf("Internal Server Error: failed to create emotion records: %v\n", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
 
 	ctx.JSON(200, gin.H{
 		"message": "success",
--- backend/internal/repository/post_trials.go
+++ backend/internal/repository/post_trials.go
@@ -1,42 +1,50 @@
 package repository
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hoka-isdl/MERS/backend/internal/schema"
 )
 
-func CreateSubjectsRecords(subject schema.PostTrialsRequestSubject) {
-	insert, err := db.Prepare("INSERT INTO Subjects(age,gender,handedness,vision,vision_aid,education,alcohol_consumption," +
-		"coffee_consumption,tea_consumption,tobacco_consumption,drug_consumption,syndroms,hours_of_sleep_last_night,normal_hours_of_sleep," +
-		"level_of_alertness,distribute_physiological_recordings,publish_audio_visual_recordings,head_circumference,distance_nasion_inion,distance_left_right_jaw_hinge) " +
-		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+func CreateSubjectsRecords(subject schema.PostTrialsRequestSubject) error {
+	insert, err := db.Prepare(`INSERT INTO subjects(age,gender,handedness,vision,vision_aid,education,alcohol_consumption,
+		coffee_consumption,tea_consumption,tobacco_consumption,drug_consumption,syndroms,hours_of_sleep_last_night,normal_hours_of_sleep,
+		level_of_alertness,distribute_physiological_recordings,publish_audio_visual_recordings,head_circumference,distance_nasion_inion,distance_left_right_jaw_hinge)
+		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
 	if err != nil {
-		log.Fatal(err.Error())
+		return fmt.Errorf("failed to prepare for a query to insert subject: %v", err)
 	}
-
-	insert.Exec(subject.Age, subject.Gender, subject.Handedness, subject.Vision, subject.VisionAid, subject.Education,
+	_, err = insert.Exec(subject.Age, subject.Gender, subject.Handedness, subject.Vision, subject.VisionAid, subject.Education,
 		subject.AlcoholConsumption, subject.CoffeeConsumption, subject.TeaConsumption, subject.TobaccoConsumption,
 		subject.DrugConsumption, subject.Syndroms, subject.HoursOfSleepLastNight, subject.NormalHoursOfSleep,
 		subject.LevelOfAlertness, subject.DistributePhysiologicalRecordings, subject.PublishAudioVisualRecordings,
 		subject.HeadCircumference, subject.DistanceNasionInion, subject.DistanceLeftRightJawHinge)
+	if err != nil {
+		return fmt.Errorf("failed to execute a query to insert subject: %v", err)
+	}
+	return nil
 }
 
-func CreateEmotionsRecords(subject schema.PostTrialsRequestSubject, rating []schema.PostTrialsRequestRatingsInner) {
-	insert, err := db.Prepare("INSERT INTO Emotions(trial_id,image_id,valence,arousal,liking,dominance,famility) VALUES(?, ?, ?, ?, ?, ?, ?)")
+func CreateEmotionsRecords(subject schema.PostTrialsRequestSubject, rating []schema.PostTrialsRequestRatingsInner) error {
+	insert, err := db.Prepare("INSERT INTO emotions(trial_id,image_id,valence,arousal,liking,dominance,famility) VALUES(?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
-		log.Fatal(err.Error())
+		return fmt.Errorf("failed to prepare for a query to insert emotion: %v", err)
 	}
-
 	for i := 0; i < len(rating); i++ {
-		insert.Exec(subject.ParticipantId, rating[i].MediaId, rating[i].Emotions.Valence,
+		if _, err := insert.Exec(
+			subject.ParticipantId, rating[i].MediaId, rating[i].Emotions.Valence,
 			rating[i].Emotions.Arousal, rating[i].Emotions.Liking,
-			rating[i].Emotions.Dominance, rating[i].Emotions.Famility)
+			rating[i].Emotions.Dominance, rating[i].Emotions.Famility,
+		); err != nil {
+			return fmt.Errorf("failed to execute a query to insert emotion: %v", err)
+		}
 	}
+	return nil
 }
 
 func CreateMediasRecords() {
-	insert, err := db.Prepare("INSERT INTO Medias(type,filename) VALUES(?, ?)")
+	insert, err := db.Prepare("INSERT INTO medias(type,filename) VALUES(?, ?)")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
