f7a45687fd43d7b4cded2b4657f43a7bf6d094aa
be3 <d_0711@icloud.com>
2023-09-17 03:32:55 +0900

    add: implement POST /trials

--- backend/internal/controller/trials.go
+++ backend/internal/controller/trials.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hoka-isdl/MERS/backend/internal/model"
 	"github.com/hoka-isdl/MERS/backend/internal/repository"
 	"github.com/hoka-isdl/MERS/backend/internal/schema"
 )
@@ -18,18 +19,66 @@ func PostTrialsHandlerFunc(ctx *gin.Context) {
 		return
 	}
 
-	// TODO: CreateTrialsRecords()を呼び出す
-	if err := repository.CreateSubjectsRecords(trials.Subject); err != nil {
+	// TODO: locationやplatformが予め定義されたものかどうかをチェックする
+	// TODO: media_typeが予め定義されたものかどうかをチェックする
+
+	trialId, err := repository.CreateTrial(trials)
+	if err != nil {
+		log.Printf("Internal Server Error: failed to create a trial record: %v\n", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	if err := repository.CreateSubject(trialId, trials.SubjectMetadata); err != nil {
 		log.Printf("Internal Server Error: failed to create a subject record: %v\n", err)
 		ctx.Status(http.StatusInternalServerError)
 		return
 	}
 
-	// TODO: CreateSignalsRecords()を呼び出す
-	// TODO: リクエストから得られるmedia_idが画像なのか動画なのかを判別する
+	// TODO: MaP1058と通信をしてsignalを作成する
+	// TODO: signal型はMaP1058のクライアントライブラリで定義されたものを使う
+	signal := model.Signal{}
+	if err := repository.CreateSignal(trialId, signal); err != nil {
+		log.Printf("Internal Server Error: failed to create a signal record: %v\n", err)
+		ctx.Status(http.StatusInternalServerError)
+		return
+	}
+
+	switch trials.RatingSet.MediaType {
+	case "image":
+		for _, rate := range trials.RatingSet.Rating {
+			imageId, err := repository.GetImageId(rate.Dataset, rate.Filename)
+			if err != nil {
+				log.Printf("Internal Server Error: failed to get image_id: %v\n", err)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+			if err := repository.CreateImageRating(trialId, imageId, rate.Emotion); err != nil {
+				log.Printf("Internal Server Error: failed to create a rate record: %v\n", err)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+		}
+	case "video":
+		for _, rate := range trials.RatingSet.Rating {
+			movieId, err := repository.GetMovieId(rate.Dataset, rate.Filename)
+			if err != nil {
+				log.Printf("Internal Server Error: failed to get movie_id: %v\n", err)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+			if err := repository.CreateMovieRating(trialId, movieId, rate.Emotion); err != nil {
+				log.Printf("Internal Server Error: failed to create a rate record: %v\n", err)
+				ctx.Status(http.StatusInternalServerError)
+				return
+			}
+		}
+	default:
+		log.Printf("Bad Request: invalid media type: %v\n", trials.RatingSet.MediaType)
+		ctx.String(http.StatusBadRequest, "invalid media type")
+	}
 
 	ctx.JSON(200, gin.H{
 		"message": "success",
 	})
-
 }
