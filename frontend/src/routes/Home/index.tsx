289e4149af0a0bde8e077ba0795b836ebfeb5dac
Imonao <n.k0831@softbank.ne.jp>
2024-07-02 13:50:05 +0900

    変更

--- frontend/src/routes/Home/index.tsx
+++ frontend/src/routes/Home/index.tsx
@@ -2,7 +2,7 @@ import { Text, Box, Center, Button } from "@chakra-ui/react";
 import { useNavigate } from 'react-router-dom';
 import "./Home.css"
 
-function Home() {
+function Home1() {
     const navigate = useNavigate()
     function onClickHome() {
         navigate('/questionnare')
@@ -11,7 +11,7 @@ function Home() {
         <div className="home_box">
             <Box h='700px' justifyContent='center' alignItems='center'>
                 <Center h='100%'>
-                    <Text textAlign="center" fontSize='5xl' color="white">MERS　</Text>
+                    <Text textAlign="center" fontSize='5xl' color="white">MERS1　</Text>
                     <Text textAlign="center" fontSize='3xl' color="white"> - Media Emotion Rating System -</Text>
                 </Center> 
             </Box>
